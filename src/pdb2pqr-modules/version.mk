60361f89918c8e0ec47b752c9152c07cff50f344
nadyawilliams <nadya@sdsc.edu>
2014-12-22 18:45:40 -0800

    add modules for 2 versions. need more testing

---
+++ src/pdb2pqr-modules/version.mk
@@ -0,0 +1,9 @@
+PACKAGE     = pdb2pqr
+CATEGORY    = applications
+NAME        = $(PACKAGE)-module
+RELEASE     = 0
+PKGROOT     = /opt/modulefiles/$(CATEGORY)
+
+RPM.REQUIRES    = environment-modules
+
+
